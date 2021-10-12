ed74cbd50c7e3af0bb282e1248e403b5b1b34963
Yuseigo5ds <92383347+Yuseigo5ds@users.noreply.github.com>
2021-10-12 10:39:35 -0400

    Update Robot_Final_Project.ino
    Added comments to code

--- Robot_Final_Project/Robot_Final_Project.ino
+++ Robot_Final_Project/Robot_Final_Project.ino
@@ -1,28 +1,28 @@
-#include "SimpleRSLK.h"
+#include "SimpleRSLK.h" //required libraries
 #include <ROMI_MOTOR_POWER.h>
 
-#define MS 1000
-#define halfSpd 50
+#define MS 1000 //conversion for milliseconds
+#define halfSpd 50 //valid values for speed range from 0-100
 #define fullSpd 100
 
 void setup() 
 {
-  setupRSLK();
-  enableMotor(RIGHT_MOTOR);
+  setupRSLK();//required to run any robot commands
+  enableMotor(RIGHT_MOTOR);//required to use the motor
   enableMotor(LEFT_MOTOR);
 }
 
 void loop() 
 {
-  setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_FORWARD);
+  setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_FORWARD);//set the motor direction to forward
   setMotorDirection(LEFT_MOTOR, MOTOR_DIR_FORWARD);
-  setMotorSpeed(RIGHT_MOTOR, halfSpd);
+  setMotorSpeed(RIGHT_MOTOR, halfSpd);//run at half speed
   setMotorSpeed(LEFT_MOTOR, halfSpd);
-  delayMicroseconds(1000*MS);
+  delayMicroseconds(1000*MS);//for 1 second
 
-  setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_BACKWARD);
+  setMotorDirection(RIGHT_MOTOR, MOTOR_DIR_BACKWARD);//set the motor direction to backward
   setMotorDirection(LEFT_MOTOR, MOTOR_DIR_BACKWARD);
-  setMotorSpeed(RIGHT_MOTOR, halfSpd);
+  setMotorSpeed(RIGHT_MOTOR, halfSpd);//run at half speed
   setMotorSpeed(LEFT_MOTOR, halfSpd);
-  delayMicroseconds(1000*MS);
+  delayMicroseconds(1000*MS);//for 1 second
 }
